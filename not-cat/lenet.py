206b955e50c32e0e548dd806b2a6ab862dc785b9
Philipp Dyck <hallo@pdyck.me>
2018-02-25 22:08:43 +0100

    Serve model

--- not-cat/lenet.py
+++ not-cat/lenet.py
@@ -1,6 +1,6 @@
 from keras.models import Sequential
 from keras.layers import Conv2D, MaxPooling2D
-from keras.layers import Activation, Dense, Flatten
+from keras.layers import Activation, Dense, Flatten, Dropout
 
 def LeNet():
     model = Sequential()
@@ -12,10 +12,12 @@ def LeNet():
     model.add(Conv2D(64, (5, 5), padding='same'))
     model.add(Activation('relu'))
     model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
+    model.add(Dropout(0.5))
 
     model.add(Flatten())
     model.add(Dense(512))
     model.add(Activation('relu'))
+    model.add(Dropout(0.5))
 
     model.add(Dense(2))
     model.add(Activation('softmax'))
---
+++ not-cat/serve.py
@@ -0,0 +1,36 @@
+from flask import Flask, request
+from keras.models import load_model
+from urllib.request import urlopen
+import numpy as np
+import cv2
+from keras.preprocessing.image import img_to_array
+
+app = Flask(__name__)
+model = load_model('cat_not_cat.h5')
+
+@app.route('/predict', methods=['GET'])
+def predict():
+    url = request.args.get('url')
+    req = urlopen(url)
+    arr = np.array(bytearray(req.read()), dtype='uint8')
+    image = cv2.imdecode(arr, cv2.IMREAD_COLOR)
+
+    image = cv2.resize(image, (28, 28))
+    image = img_to_array(image)
+    image = np.array([image], dtype='float') / 255.0
+
+    prediction = model.predict(image)
+    not_cat = prediction[0][0]
+    cat = prediction[0][1]
+
+    img_tag = '<img src="' + url + '">'
+
+    if cat > not_cat:
+        percentage = str(round(cat * 100, 2)) + '% Katze'
+    else:
+        percentage = str(round(not_cat * 100, 2)) + '% keine Katze'
+
+    return img_tag + percentage
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
